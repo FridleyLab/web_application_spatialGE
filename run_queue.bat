da1aa78eac5d1073893f117e3ad8cc55c1569aad
Roberto Manjarres-Betancur <roberto.manjarres-betancur@moffitt.org>
2023-08-02 23:07:17 -0400

    New way to know a job position in the queue

--- run_queue.bat
+++ run_queue.bat
@@ -3,5 +3,5 @@ start /B php artisan queue:work --queue="low" --timeout=3600 --tries=1
 start /B php artisan queue:work --queue="low" --timeout=3600 --tries=1
 start /B php artisan queue:work --queue="default" --timeout=3600 --tries=1
 start /B php artisan queue:work --queue="default" --timeout=3600 --tries=1
-REM start /B php artisan queue:work --queue="default" --timeout=3600 --tries=1
-REM start /B php artisan queue:work --queue="default" --timeout=3600 --tries=1
+start /B php artisan queue:work --queue="default" --timeout=3600 --tries=1
+start /B php artisan queue:work --queue="default" --timeout=3600 --tries=1
