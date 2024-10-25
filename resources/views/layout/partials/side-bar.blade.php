542dc50b6251e7cc28b150bdafcfc42663c786fd
ramanjar <ramanjar@gmail.com>
2024-10-25 10:33:53 -0400

    Hide SPARK-X in production

--- resources/views/layout/partials/side-bar.blade.php
+++ resources/views/layout/partials/side-bar.blade.php
@@ -143,6 +143,7 @@
                             </a>
                         </li>
 
+                        @if(app()->isLocal())
                         <li class="nav-item ps-2">
                             <a id="sparkx-a" class="nav-link text-white @if(url()->current() === route('sparkx', ['project' => session('project_id')])) active bg-gradient-info @endif @if(getActiveProject()->current_step < 3) disabled @endif" style="@if(getActiveProject()->current_step < 3) background-color: transparent !important @endif" href="{{ route('sparkx', ['project' => session('project_id')]) }}">
                                 <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -151,6 +152,7 @@
                                 <span id="sparkx-span" class="nav-link-text ms-1 text-xs @if(getActiveProject()->current_step < 3) text-secondary @endif text-bold">SPARK-X</span>
                             </a>
                         </li>
+                        @endif
 
                     @endif
                 </div>
