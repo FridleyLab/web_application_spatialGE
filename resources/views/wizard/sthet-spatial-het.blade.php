6e07bc3df95a81b9fb22c2ce8e1bbabafe6ee509
ramanjar <ramanjar@gmail.com>
2024-11-15 14:54:07 -0500

    Env fix

--- resources/views/wizard/sthet-spatial-het.blade.php
+++ resources/views/wizard/sthet-spatial-het.blade.php
@@ -10,7 +10,7 @@
         :color-palettes="{{ json_encode($color_palettes) }}"
 
         spark-url="{{ route('spark', ['project' => $project->id]) }}"
-        :is-local="{{ app()->environment === 'local' ? 1 : 0 }}"
+        :is-local="{{ app()->environment() === 'local' ? 1 : 0 }}"
     >
     </sthet-spatial-het>
 @endsection
