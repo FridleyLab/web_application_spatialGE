b8ad90377a5409402d74194009a886e6e000c484
ramanjar <ramanjar@gmail.com>
2024-11-15 09:06:17 -0500

    Adjustment in Expression Surface script

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2102,7 +2102,7 @@ $export_files
         $export_files = '';
         foreach ($genes as $gene)
             foreach ($this->samples as $sample)
-                $export_files .= $this->getExportFilesCommands("stplot-expression-surface-$gene-" . $sample->name, "krp\$" . $gene . "_" . $sample->name);
+                $export_files .= $this->getExportFilesCommands("stplot-expression-surface-$gene-" . $sample->name, "krp[[" . $gene . "_" . $sample->name . "]]");
 
         $script = "
 
